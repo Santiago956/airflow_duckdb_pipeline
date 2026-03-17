bcbff0df302f4e803fe392110879e9743f9d9dee
Santiago956 <ppfsantiago@gmail.com>
2026-03-17 13:51:13 -0300

    hello task

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from airflow-duckdb-pipeline!")
+from airflow.sdk import dag, task
+from datetime import datetime
 
+@dag
+def hello_dag(dag_id="hello_dag",
+              start_date=datetime(2026,3,17),
+              schedule_interval='@hourly',
+              catchup=False):
+    @task
+    def hello_task():
+        print("Hello World!")
 
-if __name__ == "__main__":
-    main()
+    task()
+
